9af7862e7f97242dcb4c4cdd0ec689ac1f93d785
Chris <csjones@users.noreply.github.com>
2018-02-10 02:00:40 -0800

    Initial source commit

---
+++ Sources/gg/root.swift
@@ -0,0 +1,19 @@
+import Guaka
+
+var rootCommand = Command(
+  usage: "gg", configuration: configuration, run: execute)
+
+private func configuration(command: Command) {
+
+  command.add(flags: [
+    // Add your flags here
+    ]
+  )
+
+  // Other configurations
+}
+
+private func execute(flags: Flags, args: [String]) {
+  // Execute code here
+  print("gg called")
+}
